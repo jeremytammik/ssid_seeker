a3afa225e47b3273cd00c11a624529d950e836d6
Jeremy Tammik <jeremy.tammik@autodesk.com>
2023-08-10 21:05:51 +0200

    removed config.step

--- jconfig.lua
+++ jconfig.lua
@@ -2,9 +2,8 @@ return {
     pin_motor_l = 9,
     pin_motor_r = 16,
     pin_switch_end = 17,
-    step = 3,
     steploop = 10000,
-    stepback = 1000,
+    stepback = 500,
     ifName = 'wlan-sta',
     debug = 1
 }
