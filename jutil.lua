585d04386ce631399df288d014a5b82c2c8306de
Jeremy Tammik <jeremy.tammik@autodesk.com>
2023-08-10 20:18:24 +0200

    improved stepback

--- jutil.lua
+++ jutil.lua
@@ -7,54 +7,50 @@ function initialize()
   gpioWrite(config.pin_motor_r, 0)
 
   gpioCreate(config.pin_switch_end, 'in')
-end -- initialize
+end
 
 function is_end_switch_on()
   return gpioRead(config.pin_switch_end) == '0'
-end -- switch_end
+end
 
 function motor_stop()
   gpioWrite(config.pin_motor_l, 0)
   gpioWrite(config.pin_motor_r, 0)
-end -- motor_stop
-
-function motor_end_switch_monitor()
-  debug('> motor_end_switch_monitor', 1)
-  while true do
-    if is_end_switch_on() then
-      motor_stop()
-    end
-    sleep(1)
-  end
 end
 
-function motor_step(direction)
-  for i = 1, config.steploop do
+-- step back a bit after hitting end sensor
+function motor_step_back(direction)
+  for i = 1, config.stepback do
     gpioWrite(direction, 1)
+  end
+  motor_stop()
+end
+
+function motor_step(directionforward, directionback)
+  for i = 1, config.steploop do
+    gpioWrite(directionforward, 1)
     if is_end_switch_on() then
       motor_stop()
+      motor_step_back(directionback)
       break
     end
   end
   motor_stop()
-end -- motor_step_r
+end
 
 function motor_step_l()
-  motor_step(config.pin_motor_l)
-end -- function motor_step_l
+  motor_step(config.pin_motor_l, config.pin_motor_r)
+end
 
 function motor_step_r()
-  motor_step(config.pin_motor_r)
-end -- function motor_step_l
+  motor_step(config.pin_motor_r, config.pin_motor_l)
+end
 
 function reset()
   while (not is_end_switch_on()) do
     gpioWrite(config.pin_motor_l, 1)
-  end -- while switch_end
-  motor_stop()
-  for i = 1, config.stepback do
-    gpioWrite(config.pin_motor_r, 1)
   end
   motor_stop()
-end -- function reset
+  motor_step_back(config.pin_motor_r)
+end
 
